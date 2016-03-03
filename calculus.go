b223da44fd28b1f0be088c4d948eb0a6df668b8d
Chris Dickerman <crdpilot@gmail.com>
2016-03-03 00:20:27 -0500

    Gaussian Quadrature

--- calculus.go
+++ calculus.go
@@ -5,15 +5,7 @@ import (
 	"fmt"
 )
 
-func SetPrec(defaultPrec int, usrPrec []int) int {
-	var prec int
-	if len(usrPrec) > 0 {
-		prec = usrPrec[0]
-	} else {
-		prec = defaultPrec
-	}
-	return prec
-}
+////////////////////////////////////Symbolic////////////////////////////////////
 
 func (fnc Function) SymDiff() string {
 	 fmt.Println(fnc.ParseToks())
@@ -37,59 +29,113 @@ func (fnc Function) SymDiff() string {
 	// return exp
 }
 
-func (fnc Function) Diff(point float64, usrPrec ...int) float64 {
+///////////////////////////////////Numerical////////////////////////////////////
+
+/*
+*	Determines how many samples to use
+*/
+func SetPrec(defaultPrec int, usrPrec []int) int {
+	var prec int
+	if len(usrPrec) > 0 {
+		prec = usrPrec[0]
+	} else {
+		prec = defaultPrec
+	}
+	return prec
+}
+
+/*
+*	Implements functions defined in code of the form: func someFunc(float64) float64
+*/
+type cFunc func(float64) float64
+
+/*
+*	Implementation of AntiDiff()
+*/
+func AntiDiff(f cFunc, a, b float64, i ...int) float64 {
+		n := SetPrec(10, i)
+    x, w := GlqNodes(n, f)
+    var sum float64
+    bma2 := (b - a) * .5
+    bpa2 := (b + a) * .5
+    for i, xi := range x {
+        sum += w[i] * f(bma2*xi+bpa2)
+    }
+    return bma2 * sum
+}
+
+func GlqNodes(n int, f cFunc) (node []float64, weight []float64) {
+    p := LegendrePoly(n)
+    pn := p[n]
+    n64 := float64(n)
+    dn := func(x float64) float64 {
+        return (x*pn(x) - p[n-1](x)) * n64 / (x*x - 1)
+    }
+    node = make([]float64, n)
+    for i := range node {
+        x0 := math.Cos(math.Pi * (float64(i+1) - .25) / (n64 + .5))
+        node[i] = NewtonRaphson(pn, dn, x0)
+    }
+    weight = make([]float64, n)
+    for i, x := range node {
+        dnx := dn(x)
+        weight[i] = 2 / ((1 - x*x) * dnx * dnx)
+    }
+    return
+}
+
+func LegendrePoly(n int) []cFunc {
+    r := make([]cFunc, n+1)
+    r[0] = func(float64) float64 { return 1 }
+    r[1] = func(x float64) float64 { return x }
+    for i := 2; i <= n; i++ {
+        i2m1 := float64(i*2 - 1)
+        im1 := float64(i - 1)
+        rm1 := r[i-1]
+        rm2 := r[i-2]
+        invi := 1 / float64(i)
+        r[i] = func(x float64) float64 {
+            return (i2m1*x*rm1(x) - im1*rm2(x)) * invi
+        }
+    }
+    return r
+}
+
+func NewtonRaphson(f, df cFunc, x0 float64) float64 {
+    for i := 0; i < 30; i++ {
+        x1 := x0 - f(x0)/df(x0)
+        if math.Abs(x1-x0) <= math.Abs(x0*1e-15) {
+            return x1
+        }
+        x0 = x1
+    }
+    panic("no convergence")
+}
+
+/*
+*	Implementation of Diff()
+*/
+func Diff(fn cFunc, point float64, usrPrec ...int) float64 {
 	prec := SetPrec(10000000, usrPrec)
 	h := 1 / float64(prec)
-	return (fnc.Eval(point+h) - fnc.Eval(point)) / h
+	return (fn(point+h) - fn(point)) / h
 }
 
+/*
+*	Wrappers for Diff and AntiDiff for evaluating the function inputted as a string with the Function struct
+*/
 func (fnc Function) AntiDiff(lower float64, upper float64, usrPrec ...int) float64 {
-	prec := SetPrec(1000, usrPrec)
-	parts := make([]float64, 2*prec+1)
-	r := upper - lower
-	nf := float64(prec)
-	dx0 := r / nf
-	parts[0] = fnc.Eval(lower) * dx0
-	parts[1] = fnc.Eval(lower+dx0*.5) * dx0 * 4
-	x0 := lower + dx0
-	for i := 1; i < prec; i++ {
-		x1 := lower + float64(i+1)*r/nf
-		xmid := (x0 + x1) * .5
-		dx := x1 - x0
-		parts[2*i] = fnc.Eval(x0) * dx * 2
-		parts[2*i+1] = fnc.Eval(xmid) * dx * 4
-		x0 = x1
+	if len(usrPrec) > 0 {
+		return AntiDiff(fnc.Eval, lower, upper, usrPrec[0])
+	} else {
+		return AntiDiff(fnc.Eval, lower, upper)
 	}
-	parts[2*prec] = fnc.Eval(upper) * dx0
-	return sum(parts) / 6
 }
 
-func sum(v []float64) float64 {
-	if len(v) == 0 {
-		return 0
+func (fnc Function) Diff(point float64, usrPrec ...int) float64 {
+	if len(usrPrec) > 0 {
+		return Diff(fnc.Eval, point, usrPrec[0])
+	} else {
+		return Diff(fnc.Eval, point)
 	}
-	var parts []float64
-	for _, x := range v {
-		var i int
-		for _, p := range parts {
-			sum := p + x
-			var err float64
-			if math.Abs(x) < math.Abs(p) {
-				err = x - (sum - p)
-			} else {
-				err = p - (sum - x)
-			}
-			if err != 0 {
-				parts[i] = err
-				i++
-			}
-			x = sum
-		}
-		parts = append(parts[:i], x)
-	}
-	var sum float64
-	for _, x := range parts {
-		sum += x
-	}
-	return sum
 }
